b12621fedd8c64516fc41b4337a46ad0d942f44f
k1aShiMa <2sillgabor@gmail.com>
2025-05-24 19:09:35 +0200

    yessir

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -6,11 +6,20 @@ import json
 import tkinter as tk
 
 
-def show_text(text):
-    user_input = entry.get()
-    if user_input:
-        text.delete(1.0, tk.END)  # Clear the text box
-        text.insert(tk.END, user_input)  # Insert the user input
+def write_to_file():
+    user_input = input("Do you want to write to a file? (yes/no): ").strip().lower()
+    if user_input == 'yes':
+        user_input = entry.get()
+        with open("encryption.txt", 'a') as file:
+            file.write(user_input + "\n")
+        label.config(text="Data written to encryption.txt")
+        print(f"Data written to {"encryption.txt"}")
+    else:
+        print("Data not written to file.")
+
+def clear_text():
+    entry.delete(0, tk.END)
+    label.config(text="Enter your text:")
 
 #create window
 window = tk.Tk()
@@ -21,12 +30,11 @@ entry = tk.Entry(window, width=30)
 entry.pack(pady=10)
 
 #button to submit input
-button = tk.Button(window, text="Submit", command = show_text)
-button2 = tk.Button(window, text="Clear", command=lambda: entry.delete(0, tk.END))
-button.pack(pady=3)
+button = tk.Button(window, text="Save", command=write_to_file)
+button.pack(pady=5)
 
 #add labels and text boxes
-label = tk.Label(window, text="Enter your text:")
+label = tk.Label(window, text="Enter the text you wanna encrypt:")
 label.pack(pady=10)
 
 window.mainloop()
