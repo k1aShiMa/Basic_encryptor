769eead68d329819eb96986871e3ceb869bad9f8
k1aShiMa <2sillgabor@gmail.com>
2025-05-24 20:05:12 +0200

    encryption

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -7,19 +7,36 @@ import tkinter as tk
 
 
 def write_to_file():
-    user_input = input("Do you want to write to a file? (yes/no): ").strip().lower()
-    if user_input == 'yes':
-        user_input = entry.get()
-        with open("encryption.txt", 'a') as file:
-            file.write(user_input + "\n")
-        label.config(text="Data written to encryption.txt")
-        print(f"Data written to {"encryption.txt"}")
-    else:
-        print("Data not written to file.")
+    user_input = entry.get()
 
+    if len(user_input) < 6:
+        label.config(text="Please enter a longer text (at least 6 characters).")
+        return
+
+    #encode the input
+    hash_key = hashlib.sha512(user_input.encode()).hexdigest()
+
+    with open("encryption.txt", 'a') as file:
+        file.write(hash_key + "\n")
+    label.config(text="Data written to encryption.txt")
+
+# Function to clear the text box and reset the label
 def clear_text():
     entry.delete(0, tk.END)
-    label.config(text="Enter your text:")
+    label.config(text="Enter the text you wanna encrypt:")
+
+# Function to exit the program
+def exit_program():
+    sys.exit()
+
+# Function to check if the file exists and read its content
+def read_file():
+    if os.path.exists("encryption.txt"):
+        with open("encryption.txt", 'r') as file:
+            content = file.read()
+        return content
+    else:
+        return "File does not exist."
 
 #create window
 window = tk.Tk()
@@ -29,9 +46,21 @@ window.geometry("400x300")
 entry = tk.Entry(window, width=30)
 entry.pack(pady=10)
 
-#button to submit input
-button = tk.Button(window, text="Save", command=write_to_file)
-button.pack(pady=5)
+#button to read the file
+read_button = tk.Button(window, text="Read encryption.txt", command=lambda: label.config(text=read_file()))
+read_button.pack(pady=10)
+
+#button to encrypt into a file
+save_button = tk.Button(window, text="Save sha512 hash", command=write_to_file)
+save_button.pack(pady=5)
+
+#button to clear the text box
+clear_text_button = tk.Button(window, text="Clear Text", command=clear_text)
+clear_text_button.pack(pady=5)
+
+#exit button
+exit_button = tk.Button(window, text="Exit", command=exit_program)
+exit_button.pack(pady=5)
 
 #add labels and text boxes
 label = tk.Label(window, text="Enter the text you wanna encrypt:")
